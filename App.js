e43f2dde021642d22cecc84543d7c4a10aa8f404
Ilia Semenko <kris22dupstepgo@gmail.com>
2021-12-22 22:20:14 +0200

    s

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
 
 export default function App() {
+  const [todoId, setTodoId] = useState(null)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
+  let content = (
+    <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo} />
+  )
+
+  if (todoId) {
+    content = <TodoScreen />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title='Нотатки' />
+      <View style={styles.container}>{content}</View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
+})
